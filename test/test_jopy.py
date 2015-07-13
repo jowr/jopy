9acbd2f43dfa81b2d27babe00d647447526a8bf7
Jorrit Wronski <jowr@users.noreply.github.com>
2015-07-14 00:35:51 +0200

    Update test_jopy.py

--- test/test_jopy.py
+++ test/test_jopy.py
@@ -29,17 +29,17 @@ class TestJopyRecip(object):
         bo = 0.05
         pp = 0.005
         cv = 10e-6
-        exp.set_geometry(cr,cl,bo,pp,cv)
-        imp.set_geometry(cr,cl,bo,pp,cv)
+        TestJopyRecip.exp.set_geometry(cr,cl,bo,pp,cv)
+        TestJopyRecip.imp.set_geometry(cr,cl,bo,pp,cv)
         pass
 
     def test_recip_objects(self):
-        assert not isinstance(exp, RecipImplicit)
-        assert isinstance(imp, RecipBase)
+        assert not isinstance(TestJopyRecip.exp, RecipImplicit)
+        assert isinstance(TestJopyRecip.imp, RecipBase)
         
     def test_recip_functions(self):
-        rev = exp.revolution(100)
-        assert np.max(np.abs(exp.volume(rev)-imp.volume(rev)))<1e-10
+        rev = TestJopyRecip.exp.revolution(100)
+        assert np.max(np.abs(TestJopyRecip.exp.volume(rev)-TestJopyRecip.imp.volume(rev)))<1e-10
 
     @classmethod
     def teardown_class(cls):
