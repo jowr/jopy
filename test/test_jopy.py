57f8e3e21fb51a1fc41f184750040dd71bcab275
Jorrit Wronski <jowr@users.noreply.github.com>
2015-07-14 00:29:55 +0200

    Update test_jopy.py

--- test/test_jopy.py
+++ test/test_jopy.py
@@ -1,7 +1,8 @@
 """
 Tests for `jopy` module.
 """
-from jopy.recip import RecipMachine, RecipMachineAlt
+from jopy.recip import RecipExplicit, RecipImplicit
+import numpy as np
 
 
 class TestJopy(object):
@@ -18,16 +19,27 @@ class TestJopy(object):
         pass
 
 class TestJopyRecip(object):
+    exp = RecipExplicit()
+    imp = RecipImplicit()
 
     @classmethod
     def setup_class(cls):
+        cr = 0.05
+        cl = 0.15
+        bo = 0.05
+        pp = 0.005
+        cv = 10e-6
+        exp.set_geometry(cr,cl,bo,pp,cv)
+        imp.set_geometry(cr,cl,bo,pp,cv)
         pass
 
-    def test_something(self):
-        obj = RecipMachine()
-        alt = RecipMachineAlt()
-        assert not isinstance(obj, RecipMachineAlt)
-        assert isinstance(alt, RecipMachine)
+    def test_recip_objects(self):
+        assert not isinstance(exp, RecipImplicit)
+        assert isinstance(imp, RecipBase)
+        
+    def test_recip_functions(self):
+        rev = exp.revolution(100)
+        assert np.max(np.abs(exp.volume(rev)-imp.volume(rev)))<1e-10
 
     @classmethod
     def teardown_class(cls):
